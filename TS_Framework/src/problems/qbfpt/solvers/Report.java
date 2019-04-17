102c9dec5e8dc8898282dc6aef33d610ca132653
Patrick Vasconcelos <patrickvs@hotmail.com>
2019-04-17 02:54:58 -0300

    Adição da flag isBestImproving

--- TS_Framework/src/problems/qbfpt/solvers/Report.java
+++ TS_Framework/src/problems/qbfpt/solvers/Report.java
@@ -3,58 +3,58 @@ package problems.qbfpt.solvers;
 import problems.log.Log;
 
 public class Report {
-	
-	static String[] INSTANCES = {
-			"instances/qbf020",
-			"instances/qbf040",
-			"instances/qbf060",
-			"instances/qbf080",
-			"instances/qbf100",
-			"instances/qbf200",
-			"instances/qbf400",
-		}; 
-	
-	static Integer[] INTERATIONS = {
-			1000000,
-			1000000,
-			10000000,
-			10000000,
-			100000000,
-			100000000,
-			1000000000,
-		}; 
-	
-	static Integer[] TENURE = {
-			20,
-			100
-	};
-	
-	
+
+	static String[] INSTANCES = { "instances/qbf020", "instances/qbf040", "instances/qbf060", "instances/qbf080",
+			"instances/qbf100", "instances/qbf200", "instances/qbf400", };
+
+	static Integer[] INTERATIONS = { 1000000, 1000000, 10000000, 10000000, 100000000, 100000000, 1000000000, };
+
+	static Integer[] TENURE = { 20, 100 };
+
+	static float[] PROBABILITY = { (float) 0.25, (float) 0.50, (float) 0.75 };
+
 	public static void main(String[] args) {
-		
+
 		for (int i = 0; i < INSTANCES.length; i++) {
-			Log.geLogger().info("Instancia: "+INSTANCES[i]);
-			Log.geLogger().info("Interações: "+INTERATIONS[i]);
+			Log.geLogger().info("Instancia: " + INSTANCES[i]);
+			Log.geLogger().info("Interações: " + INTERATIONS[i]);
 			for (int j = 0; j < TENURE.length; j++) {
-				Log.geLogger().info("Tamanho Tabu: "+TENURE[j]);
-				try {
-					Log.geLogger().info("Algoritmo: Padrão");
-					Log.geLogger().info("Best Improving: SIM");
-					new TS_QBFPT(TENURE[j], INTERATIONS[i], INSTANCES[i], Boolean.TRUE).solve();
-					Log.geLogger().info("Best Improving: NÂO");
-					new TS_QBFPT(TENURE[j], INTERATIONS[i], INSTANCES[i], Boolean.FALSE).solve();
-					Log.geLogger().info("Algoritmo: Diversification > Limite = "+1000);
-					Log.geLogger().info("Best Improving: SIM");
-					new TS_QBFPT_Diversification(TENURE[j], INTERATIONS[i], INSTANCES[i],  Boolean.TRUE, 1000).solve();
-					Log.geLogger().info("Best Improving: NÂO");
-					new TS_QBFPT_Diversification(TENURE[j], INTERATIONS[i], INSTANCES[i],  Boolean.TRUE, 1000).solve();
-				} catch (Throwable e) {
-					Log.geLogger().info("Erro");
+				Log.geLogger().info("Tamanho Tabu: " + TENURE[j]);
+				/*
+				 * try {
+				 * 
+				 * Log.geLogger().info("Algoritmo: Padrão");
+				 * Log.geLogger().info("Best Improving: SIM"); new TS_QBFPT(TENURE[j],
+				 * INTERATIONS[i], INSTANCES[i], Boolean.TRUE).solve();
+				 * Log.geLogger().info("Best Improving: NÂO"); new TS_QBFPT(TENURE[j],
+				 * INTERATIONS[i], INSTANCES[i], Boolean.FALSE).solve();
+				 * Log.geLogger().info("Algoritmo: Diversification > Limite = "+1000);
+				 * Log.geLogger().info("Best Improving: SIM"); new
+				 * TS_QBFPT_Diversification(TENURE[j], INTERATIONS[i], INSTANCES[i],
+				 * Boolean.TRUE, 1000).solve(); Log.geLogger().info("Best Improving: NÂO"); new
+				 * TS_QBFPT_Diversification(TENURE[j], INTERATIONS[i], INSTANCES[i],
+				 * Boolean.TRUE, 1000).solve();
+				 */
+
+				for (int k = 0; k < PROBABILITY.length; k++) {
+					Log.geLogger().info("Probabilidade: " + PROBABILITY[k]);
+					try {
+						Log.geLogger().info("Best Improving: SIM");
+						new TS_QBFPT_Probabilistic(TENURE[j], INTERATIONS[i], INSTANCES[i], Boolean.TRUE,
+								PROBABILITY[k]).solve();
+						Log.geLogger().info("Best Improving: NÂO");
+						new TS_QBFPT_Probabilistic(TENURE[j], INTERATIONS[i], INSTANCES[i], Boolean.FALSE,
+								PROBABILITY[k]).solve();
+
+					}
+
+					catch (Throwable e) {
+						Log.geLogger().info("Erro");
+					}
 				}
-				
 			}
-			
+
 		}
-		
+
 	}
 }
--- TS_Framework/src/problems/qbfpt/solvers/TS_QBFPT_Probabilistic.java
+++ TS_Framework/src/problems/qbfpt/solvers/TS_QBFPT_Probabilistic.java
@@ -9,27 +9,32 @@ import problems.qbfpt.triples.ForbiddenTriplesBuilder;
 import solutions.Solution;
 
 public class TS_QBFPT_Probabilistic extends TS_QBF {
-	
+
 	private ForbiddenTriplesBuilder ftBuilder;
 
 	private float probabilityParameter;
-	public TS_QBFPT_Probabilistic(Integer tenure, Integer iterations, String filename, float probability) throws IOException {
+	private boolean isBestImproving;
+
+	public TS_QBFPT_Probabilistic(Integer tenure, Integer iterations, String filename, boolean isBestImproving,
+			float probability) throws IOException {
 		super(tenure, iterations, filename);
 		this.ftBuilder = new ForbiddenTriplesBuilder(ObjFunction.getDomainSize());
 		this.probabilityParameter = probability;
+		this.isBestImproving = isBestImproving;
 	}
-	
+
 	@Override
 	public void updateCL() {
 		if (!this.incumbentSol.isEmpty()) {
 			List<Integer> forbiddenValues = new ArrayList<>();
-			Integer lastElem = this.incumbentSol.get(this.incumbentSol.size()-1);
-			for (int i = 0; i < this.incumbentSol.size()-1; i++) {
-				forbiddenValues.addAll(ftBuilder.getForbiddenValues(this.incumbentSol.get(i)+1, lastElem+1));
+			Integer lastElem = this.incumbentSol.get(this.incumbentSol.size() - 1);
+			for (int i = 0; i < this.incumbentSol.size() - 1; i++) {
+				forbiddenValues.addAll(ftBuilder.getForbiddenValues(this.incumbentSol.get(i) + 1, lastElem + 1));
 			}
 			for (Integer fv : forbiddenValues) {
-				int index = CL.indexOf(fv-1);
-				if (index >= 0) CL.remove(index);
+				int index = CL.indexOf(fv - 1);
+				if (index >= 0)
+					CL.remove(index);
 			}
 		}
 	}
@@ -40,25 +45,27 @@ public class TS_QBFPT_Probabilistic extends TS_QBF {
 		Double minDeltaCost;
 		Integer bestCandIn = null, bestCandOut = null;
 		Double pBestCandIn = null, pBestCandOut = null;
-		ArrayList<Double> pIn = new ArrayList<Double>(); 
-		ArrayList<Double> pOut = new ArrayList<Double>(); 
+		ArrayList<Double> pIn = new ArrayList<Double>();
+		ArrayList<Double> pOut = new ArrayList<Double>();
 		minDeltaCost = Double.POSITIVE_INFINITY;
 		updateCL();
-		
-		
+
 		// Evaluate insertions
-		
+
 		for (Integer candIn : CL) {
 			double x = rng.nextFloat();
 			pIn.add(x);
 			if (x < probabilityParameter) {
 				Double deltaCost = ObjFunction.evaluateInsertionCost(candIn, incumbentSol);
-				if (!TL.contains(candIn) || incumbentSol.cost+deltaCost < bestSol.cost) {
+				if (!TL.contains(candIn) || incumbentSol.cost + deltaCost < bestSol.cost) {
 					if (deltaCost < minDeltaCost) {
 						minDeltaCost = deltaCost;
 						bestCandIn = candIn;
 						pBestCandIn = x;
 						bestCandOut = null;
+						if (!isBestImproving) {
+							break;
+						}
 					}
 				}
 			}
@@ -69,36 +76,43 @@ public class TS_QBFPT_Probabilistic extends TS_QBF {
 			pOut.add(x);
 			if (x < probabilityParameter) {
 				Double deltaCost = ObjFunction.evaluateRemovalCost(candOut, incumbentSol);
-				if (!TL.contains(candOut) || incumbentSol.cost+deltaCost < bestSol.cost) {
+				if (!TL.contains(candOut) || incumbentSol.cost + deltaCost < bestSol.cost) {
 					if (deltaCost < minDeltaCost) {
 						minDeltaCost = deltaCost;
 						bestCandIn = null;
 						bestCandOut = candOut;
 						pBestCandOut = x;
-					}
-				}
-			}
-		}
-		// Evaluate exchanges
-		for (int i = 0; i < CL.size();i++){
-			Integer candIn = CL.get(i);
-			if(pIn.get(i)< probabilityParameter){
-				for (int j = 0; j < incumbentSol.size();j++){
-					Integer candOut = incumbentSol.get(j);
-					if(pOut.get(i)< probabilityParameter){
-					Double deltaCost = ObjFunction.evaluateExchangeCost(candIn, candOut, incumbentSol);
-					if ((!TL.contains(candIn) && !TL.contains(candOut)) || incumbentSol.cost+deltaCost < bestSol.cost) {
-						if (deltaCost < minDeltaCost) {
-							minDeltaCost = deltaCost;
-							bestCandIn = candIn;
-							bestCandOut = candOut;
-							pBestCandIn = pIn.get(i);
-							pBestCandOut = pOut.get(i);
+						if (!isBestImproving) {
+							break;
 						}
 					}
 				}
 			}
 		}
+		// Evaluate exchanges
+		for (int i = 0; i < CL.size(); i++) {
+			Integer candIn = CL.get(i);
+			if (pIn.get(i) < probabilityParameter) {
+				for (int j = 0; j < incumbentSol.size(); j++) {
+					Integer candOut = incumbentSol.get(j);
+					if (pOut.get(i) < probabilityParameter) {
+						Double deltaCost = ObjFunction.evaluateExchangeCost(candIn, candOut, incumbentSol);
+						if ((!TL.contains(candIn) && !TL.contains(candOut))
+								|| incumbentSol.cost + deltaCost < bestSol.cost) {
+							if (deltaCost < minDeltaCost) {
+								minDeltaCost = deltaCost;
+								bestCandIn = candIn;
+								bestCandOut = candOut;
+								pBestCandIn = pIn.get(i);
+								pBestCandOut = pOut.get(i);
+								if (!isBestImproving) {
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 		// Implement the best non-tabu move
 		TL.poll();
@@ -120,21 +134,19 @@ public class TS_QBFPT_Probabilistic extends TS_QBF {
 			TL.add(fake);
 		}
 		ObjFunction.evaluate(incumbentSol);
-		
+
 		return null;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		long startTime = System.currentTimeMillis();
-		TS_QBF tabusearch = new TS_QBFPT_Probabilistic(10, 1000000, "TS_Framework/instances/qbf020",(float) 0.5);
+		TS_QBF tabusearch = new TS_QBFPT_Probabilistic(10, 1000000, "instances/qbf020", Boolean.TRUE, (float) 0.25);
 		Solution<Integer> bestSol = tabusearch.solve();
 		System.out.println("maxVal = " + bestSol);
-		long endTime   = System.currentTimeMillis();
+		long endTime = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
-		System.out.println("Time = "+(double)totalTime/(double)1000+" seg");
-		
+		System.out.println("Time = " + (double) totalTime / (double) 1000 + " seg");
+
 	}
-	
-	
 
 }
